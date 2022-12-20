55b9df0962cbdea23ff6d8d2fad0fd3db6e8184b
Jonathan <jonathanwillianevo@hotmail.com>
2022-12-20 11:32:39 -0300

    Adicionado gitignore para arquivos com APIKEY

---
+++ lib/constants.example.dart
@@ -0,0 +1,24 @@
+class ApiConstants {
+  static String baseUrl = 'https://api.openai.com/v1/images';
+  static String generationsEndpoint = '/generations';
+  static String secretKey = 'YOUR_OPENIA_API_KEY_HERE';
+}
+
+class RandomPrompts {
+  static List<String> promptsList = [
+    'a white siamese cat',
+    'Pixar style 3D render of a baby hippo, 4k, high resolution, trending in artstation',
+    'Pixar style 3D render of a fox wearing a tophat, 4k, high resolution, trending in artstation',
+    'An oil painting of a mechanical clockwork flying machine from the renaissance, Gorgeous digital painting, amazing art, artstation 3, realistic',
+    'fantasy tavern interior | Breath-taking digital painting with warm colours amazing art mesmerizing, captivating, artstation 3',
+    'Cluttered house in the woods | anime oil painting high resolution cottagecore ghibli inspired 4k',
+    'German palace gardens and towers | oil painting, trending in artstation, anime',
+    'The green idyllic Arcadian prairie with sheep by Thomas Cole, Breath-taking digital painting with placid colours, amazing art, artstation 3, cottagecore',
+    'A digital illustration of glowing toadstools beside a pond with lilypads, 4k, detailed, trending in artstation',
+    'A digital illustration of a medieval town, 4k, detailed, trending in artstation, fantasy'
+    'A digital illustration of the Babel tower, 4k, detailed, trending in artstation, fantasy vivid colors'
+    'A digital illustration of a steampunk library with clockwork machines, 4k, detailed, trending in artstation, fantasy vivid colors'
+    'A digital illustration of Lothlorien houses on treetops with fireflies, 4k, detailed, trending in artstation, fantasy vivid colors, organic and bent',
+    'The white fox in the Arcadian praerie | Breath-taking digital painting with vivid colours amazing art mesmerizing, captivating, artstation 3, japanese style',
+  ];
+}
